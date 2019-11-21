ddfe0214f2f84db514c653f8e30099757d8798e5
Ivan <devskyfly@gmail.com>
2019-11-21 14:17:37 +0300

    Mv appFunc to app dir.

--- RoboFile.php
+++ RoboFile.php
@@ -11,12 +11,27 @@ class RoboFile extends \Robo\Tasks
         $task = $this->taskExec(__DIR__.'/vendor/bin/codecept run unit')->run();
     }
 
-    public function testsFunctional($opts = ['server|s' => false])
+    public function testsFunctional($port = 3000)
     {
-        if ($opts['server']) {
-            $this->serverRun();
-        }
-        $task = $this->taskExec(__DIR__.'/vendor/bin/codecept run functional')->run();
+        $collection = $this->collectionBuilder();
+        $collection
+        ->addTask($this->taskServer($port)
+            ->background()
+            ->dir(__DIR__.'/tests/appFunc/web/')
+        );
+        /*if ($port) {
+            $server = $this->taskServer($port)
+            ->dir(__DIR__.'/tests/appFunc/web/')
+            //->background()
+            ->run();
+        }*/
+        $collection->addTask($this->taskExec(__DIR__.'/vendor/bin/codecept run functional'));
+        $collection->run();
+
+        //if ($server->wasSuccessful()) {
+            //$task = $this->taskExec(__DIR__.'/vendor/bin/codecept run functional')->run();
+            //$server->stop();
+        //}
     }
 
     public function testsAcceptence()
@@ -24,8 +39,29 @@ class RoboFile extends \Robo\Tasks
         $task = $this->taskExec(__DIR__.'/vendor/bin/codecept run unit')->run();
     }
 
-    public function serverRun($dir = 'tests/appFunc/web', $port = 3000)
+    /*public function serverRun($dir = 'tests/appFunc/web', $port = 3000)
     {
-        $this->taskServer($port)->dir($dir)->background()->run();
+        return $this->taskServer($port)->dir($dir)->background()->run();
+    }*/
+
+    protected $steps = 10;
+
+    public function progressIndicatorSteps()
+    {
+        return $this->steps;
+    }
+
+    public function run()
+    {
+        $exitCode = 0;
+        $errorMessage = "";
+
+        $this->startProgressIndicator();
+        for ($i = 0; $i < $this->steps; ++$i) {
+            $this->advanceProgressIndicator();
+        }
+        $this->stopProgressIndicator();
+
+        return new Result($this, $exitCode, $errorMessage, ['time' => $this->getExecutionTime()]);
     }
 }
